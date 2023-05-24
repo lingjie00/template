7c9d3fb97e43cd8d3aa134f7aca8943933744838
ling <64540764+lingjie00@users.noreply.github.com>
2023-05-24 11:39:38 +0800

    added package

--- docker/dev/install/package.sh
+++ docker/dev/install/package.sh
@@ -2,7 +2,7 @@
 
 # install packages
 apt-get update && apt-get -y upgrade
-apt-get install -y fish gcc vim make ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl npm tmux
+apt-get install -y fish gcc vim make ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl npm tmux ripgrep
 apt-get clean
 
 conda init fish
