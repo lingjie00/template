60d4faaf76db35102a0081eb6b52a23b63b9b91b
ling <64540764+lingjie00@users.noreply.github.com>
2023-05-29 21:53:15 +0800

    provide a more seamless experience with dev docker

--- docker/dev/install/config.sh
+++ docker/dev/install/config.sh
@@ -5,11 +5,13 @@ git clone --depth 1 https://github.com/lingjie00/cli_settings $HOME/.dotfiles
 cd $HOME/.dotfiles
 bash $HOME/.dotfiles/symlink y n
 
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerCompile'
+
 # enable fish vim bidings
 fish_vi_key_bindings
 
 # instal plugin manager for tmux and fish
-git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && $HOME/.tmux/plugins/tpm/bin/install_plugins
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
 bash
--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -8,6 +8,14 @@ git pull
 # change to code folder
 cd /code/
 
-pip install -e .
+if [ -f "setup.py" ]; then
+    pip install -e .
+fi
 
+# start a new tmux session
+tmux new -d
+tmux send-keys "nvim" Enter
+tmux split-window -v -p 10
+
+# start a new terminal, in case runs are started from a tmux session
 fish
