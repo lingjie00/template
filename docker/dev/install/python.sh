72b2b8814c37b0fd8298a7436096092d9ca41263
ling <64540764+lingjie00@users.noreply.github.com>
2023-09-15 15:54:02 +0800

    added in python debuggers

--- docker/dev/install/python.sh
+++ docker/dev/install/python.sh
@@ -5,7 +5,7 @@ conda install -y python=$PYTHON_VERSION
 conda install -y ipython seaborn
 pip install --no-cache-dir flake8 flake8-import-order flake8-builtins flake8-eradicate flake8-quotes\
     flake8-variables-names pep8-naming flake8-docstrings flake8-tidy-imports flake8-print flake8-json\
-    pytest coverage pytest-cov pipreqs
+    pytest coverage pytest-cov pipreqs debugpy
 conda config --add channels conda-forge
 
 conda clean -a --yes
